b393bdc468ce98ce7a9f327ec26c784107f6c1a4
Gilles Benichou <skierfou@gmail.com>
2021-12-28 18:29:13 +0200

    save

---
+++ PROJECTS/gocode-shop/src/components/Products/Products.js
@@ -0,0 +1,12 @@
+import Product from "../Product/Product";
+import "./Products.css";
+
+export default function Products({ products }) {
+  return (
+    <section className="products">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </section>
+  );
+}
--- lesson-17-react-1/main.js
+++ lesson-17-react-1/main.js
@@ -10,8 +10,33 @@ const SimpleElement = () => (
 );
 ReactDOM.render(SimpleElement, document.getElementById("app"));
 
+function Todo({ id, title }) {
+  return (
+    <div>
+      {id} {title}
+    </div>
+  );
+}
 function Tasks() {
   const tasks = ["one", "two", "three"];
   const listItems = tasks.map((task) => <li>{task}</li>);
   return <ul>{listItems}</ul>;
+}
+
+function Todos() {
+  const todos = [
+    { id: 1, title: "wash", completed: false },
+    { id: 2, title: "throw", completed: false },
+  ];
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <Todo key={todo.id}>
+          {todo.id}
+          {todo.title}
+        </Todo>
+      ))}
+    </ul>
+  );
 }
